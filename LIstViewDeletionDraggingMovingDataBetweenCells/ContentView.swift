5837a7bd98c23f8dbbe1ed3a8ffb6e31d383ea86
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-10 10:47:13 +0300

    init

--- LIstViewDeletionDraggingMovingDataBetweenCells/ContentView.swift
+++ LIstViewDeletionDraggingMovingDataBetweenCells/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        cust()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct cust: View {
+    @State var data = ["1", "2", "3", "4", "5", "6"]
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(data, id: \.self) { i in
+                    Text(i)
+                }.onDelete(perform: { (index) in
+                    
+                    self.data.remove(atOffsets: index)
+                    
+                }).onMove(perform: { (index, destination) in
+                    
+                    self.data.move(fromOffsets: index, toOffset: destination)
+                    
+                })
+            }.navigationBarTitle("ListView")
+            .navigationBarItems(trailing: EditButton())
+        }
     }
 }
